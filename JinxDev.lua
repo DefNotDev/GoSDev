8eec6fef56d4d2a63d101c090ff59ae5860c11ad
DefNotDev <hellokappa8@gmail.com>
2016-08-28 14:57:13 -0300

    Add files via upload

---
+++ JinxDev.lua
@@ -0,0 +1,31 @@
+--If my hero is Jinx, return. Else, not return any.
+if GetObjectName(GetMyHero()) ~= "Jinx" then return end
+
+require("OpenPredict")
+
+
+--Menu
+local JinxMenu = Menu("Jinx", "Jinx by DefNotDev")
+JinxMenu:SubMenu("Combo", "Jinx Combo")
+JinxMenu.Combo:Boolean("Q", "Use Q on Combo", true)
+JinxMenu.Combo:Boolean("W", "Use W on Combo", true)
+JinxMenu.Combo:Boolean("E", "Use E on Combo", true)
+JinxMenu.Combo:Boolean("R", "Use R on Combo", true)
+
+--Informations
+local W = {delay = 600, speed = 3300, range = GetCastRange(myHero,_W)}
+local R = {delay = 600, speed = 1700, range = GetCastRange(myHero,_R)}
+
+
+--Target declaration
+local target = GetCurrentTarget()
+
+
+
+--Function
+function JinxCombo()
+if IOW:Mode() == "Combo" then
+
+if JinxMenu.Combo.Q:Value() and Ready(_Q) and ValidTarget(target, 1000)
+CastTargetSpell(target, _Q)
+
